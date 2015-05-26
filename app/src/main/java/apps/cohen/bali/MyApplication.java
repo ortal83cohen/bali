cf829377a95448293e01ee727df8832451d2eb0c
Ortal Cohen <ortal83cohen@gmail.com>
2015-05-26 23:42:23 +0300

    1

---
+++ app/src/main/java/apps/cohen/bali/MyApplication.java
@@ -0,0 +1,44 @@
+package apps.cohen.bali;
+
+import android.app.Application;
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.preference.PreferenceManager;
+
+
+public class MyApplication extends Application {
+
+
+    public static final String API_KEY_ROTTEN_TOMATOES = "54wzfswsa4qmjg8hjwa64d4c";
+    private static MyApplication sInstance;
+
+
+
+    public static MyApplication getInstance() {
+        return sInstance;
+    }
+
+    public static Context getAppContext() {
+        return sInstance.getApplicationContext();
+    }
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        sInstance = this;
+    }
+
+    public static void saveToPreferences(Context context, String preferenceName, String preferenceValue) {
+        SharedPreferences sharedPreferences = PreferenceManager
+                .getDefaultSharedPreferences(context.getApplicationContext());
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString(preferenceName, preferenceValue);
+        editor.apply();
+    }
+
+    public static String readFromPreferences(Context context, String preferenceName, String defaultValue) {
+        SharedPreferences sharedPreferences = PreferenceManager
+                .getDefaultSharedPreferences(context.getApplicationContext());
+        return sharedPreferences.getString(preferenceName, defaultValue);
+    }
+}
---
+++ app/src/main/java/apps/cohen/bali/pojo/Information.java
@@ -0,0 +1,9 @@
+package apps.cohen.bali.pojo;
+
+/**
+ * Created by Windows on 22-12-2014.
+ */
+public class Information {
+    public int iconId;
+    public String title;
+}
